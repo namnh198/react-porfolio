d974aac752b116c852da17d8b2e1822b710ee54d
namnh198 <namnhn98@gmail.com>
2023-05-11 14:56:39 +0700

    porfolio website

---
+++ src/data/WorkData.js
@@ -0,0 +1,53 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: 'Jewelly e-Commerce Platform',
+    description:
+      'Developed jewelry eCommerce platform using Magento, Elasticsearch, and KnockoutJS, delivering seamless shopping experience and advanced search functionality.',
+    tags: ['magento', 'elasticsearch', 'knockoutjs', 'jquery', 'less'],
+    demo: 'https://glamira.co.uk/',
+  },
+  {
+    id: 2,
+    name: 'Dropship Platform',
+    description:
+      'Built a dropship eCommerce platform with Magento, Elasticsearch, and KnockoutJS, ensuring efficient order management and seamless inventory synchronization.',
+    tags: ['magento', 'elasticsearch', 'knockoutjs', 'jquery', 'less'],
+    demo: 'https://sellkorea.com/',
+  },
+  {
+    id: 3,
+    name: 'Build API for POS Systems',
+    description:
+      'Designed and implemented the API architecture, adhering to industry best practices and standards for security, scalability, and performance.',
+    tags: ['magento', 'jquery', 'less'],
+  },
+  {
+    id: 4,
+    name: 'SMS Notification Extension',
+    description:
+      'Developed SMS notification extensions for Magento, enhancing customer engagement and order updates through real-time SMS notifications.',
+    tags: ['magento', 'jquery', 'less'],
+    demo: 'https://mage.tieugum.com/',
+    github: 'https://github.com/namnh198/magento-sms-notification',
+  },
+  {
+    id: 5,
+    name: 'Mobile Login Extension',
+    description:
+      'Implemented Magento extensions enabling seamless login with phone numbers, enhancing user experience and streamlining authentication processes.',
+    tags: ['magento', 'jquery', 'less'],
+    demo: 'https://mage.tieugum.com/',
+    github: 'https://github.com/namnh198/magento-mobile-login',
+  },
+  {
+    id: 6,
+    name: 'Microsoft Clarity Extension',
+    description:
+      'Created Magento extensions for tracking user behavior with Microsoft Clarity, providing valuable insights for website optimization and user experience enhancement.',
+    tags: ['magento', 'javascript', 'less'],
+    github: 'https://github.com/namnh198/magento-microsoft-clarity',
+  },
+];
---
+++ src/subComponents/SocialIcons.js
@@ -0,0 +1,106 @@
+import { motion } from "framer-motion";
+import React from "react";
+import styled from "styled-components";
+import { Facebook, Github, LinkedIn } from "../components/AllSvgs";
+import { DarkTheme } from "../components/Themes";
+
+const Icons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: fixed;
+  bottom: 0;
+  left: 2rem;
+
+  z-index: 3;
+
+  & > *:not(:last-child) {
+    margin: 0.5rem 0;
+  }
+`;
+
+const Line = styled(motion.span)`
+  width: 2px;
+  height: 8rem;
+  background-color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+`;
+
+const SocialIcons = (props) => {
+  return (
+    <Icons>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          rel="noreferrer"
+          href={"https://github.com/namnh198/"}
+        >
+          <Github
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.4 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          rel="noreferrer"
+          href={"https://www.facebook.com/namnh198/"}
+        >
+          <Facebook
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.6 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          rel="noreferrer"
+          href={"https://www.linkedin.com/in/tieugum/"}
+        >
+          <LinkedIn
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+
+      <Line
+        color={props.theme}
+        initial={{
+          height: 0,
+        }}
+        animate={{
+          height: "8rem",
+        }}
+        transition={{
+          type: "spring",
+          duration: 1,
+          delay: 0.8,
+        }}
+      />
+    </Icons>
+  );
+};
+
+export default SocialIcons;
